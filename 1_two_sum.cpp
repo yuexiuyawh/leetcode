0fedadcc6116ce8e30d904f67bd20df068253053
yuexiuyawh <1173931023@qq.com>
2017-05-23 17:54:43 +0800

    Create 1_two_sum.cpp

---
+++ 1_two_sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& num, int target) {
+        unordered_map<int,int> mapping;
+        vector<int> result;
+        for(int i =0; i< num.size(); i++){
+            mapping[num[i]] = i; 
+        }
+        for(int i = 0; i < num.size(); i++){
+            const int gap = target - num[i];
+            if(mapping.find(gap) != mapping.end() && mapping[gap] != i){
+                result.push_back(i);
+                result.push_back(mapping[gap]);
+                break;
+            }
+        }
+    return result; 
+    }
+};

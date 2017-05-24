1d3005724c921c68762027a729bc6ee4e6c674bc
yuexiuyawh <1173931023@qq.com>
2017-05-24 22:49:47 +0800

    Update 476_Number_Complement.cpp

--- 476_Number_Complement.cpp
+++ 476_Number_Complement.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     int findComplement(int num) {
+        int mask=1,temp=num;
+        while(temp!=0)
+        {
+            mask<<=1;
+            temp>>=1;
+        }
+        return num^(mask-1);
+    }
+};
+
+class Solution {
+public:
+    int findComplement(int num) {
+        unsigned int mask=~0;
+        while(mask&num) mask<<=1;
+        return ~mask&~num;
+    }
+};
+
+class Solution {
+public:
+    int findComplement(int num) {
         vector<int> bin(32,0);
         int i=0,res=0;
         if(num==0) return 1;

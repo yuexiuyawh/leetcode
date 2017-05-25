ef939d63db948aafdc157d90e4e629d0536bd226
yuexiuyawh <1173931023@qq.com>
2017-05-25 23:49:32 +0800

    Update 557_Reverse_Words_in_a_String_III.cpp

--- 557_Reverse_Words_in_a_String_III.cpp
+++ 557_Reverse_Words_in_a_String_III.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     string reverseWords(string s) {
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]!=' ')
+            {
+                int j;
+                for(j=i;j<s.size()&&s[j]!=' ';j++);
+                reverse(s.begin()+i,s.begin()+j);
+                i=j;
+            }
+        }
+        return s;
+    }
+};
+
+//堆栈
+class Solution {
+public:
+    string reverseWords(string s) {
         stack<char> word;
         int i=0;
         string res;

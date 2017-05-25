c05d45acd06aa66db490b5489cc5e3a720a1834b
yuexiuyawh <1173931023@qq.com>
2017-05-25 15:51:48 +0800

    Update 461_Hamming_Distance.cpp

--- 461_Hamming_Distance.cpp
+++ 461_Hamming_Distance.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     int hammingDistance(int x, int y) {
+        int z=x^y,count=0;
+        while(z)
+        {
+            if(z&1) count++;
+            z>>=1;
+        }
+        return count;
+    }
+};
+
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
         int count=0;
         for(int i=0;x!=0||y!=0;i++)
         {

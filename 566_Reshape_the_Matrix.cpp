c57402aa80c9afb99ea54151453bc727fad58dd3
yuexiuyawh <1173931023@qq.com>
2017-05-25 17:16:16 +0800

    Update 566_Reshape_the_Matrix.cpp

--- 566_Reshape_the_Matrix.cpp
+++ 566_Reshape_the_Matrix.cpp
@@ -1,3 +1,20 @@
+//一维数组
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c) {
+        int row=nums.size(),col=nums[0].size(),total=row*col;
+        if(r*c!=total)
+            return nums;
+        vector<vector<int>> res(r,vector<int>(c));
+        for(int i=0;i<total;i++)
+        {
+            res[i/c][i%c]=nums[i/col][i%col];
+        }
+        return res;
+    }
+};
+
+//以输出为中心
 class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c) {
@@ -10,6 +27,26 @@ public:
             {
                 int k=i*c+j;
                 res[i].push_back(nums[k/nums[0].size()][k%nums[0].size()]);
+            }
+        }
+        return res;
+    }
+};
+
+//以输入为中心
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c) {
+        if(r*c!=nums.size()*nums[0].size())
+            return nums;
+        vector<vector<int>> res(r);
+        int count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=0;j<nums[0].size();j++)
+            {
+                res[count/c].push_back(nums[i][j]);
+                count++;
             }
         }
         return res;

81ef776ca69244b14bb16773bcbd89b55d51952d
yuexiuyawh <1173931023@qq.com>
2017-05-26 16:27:01 +0800

    Update 575_Distribute_Candies.cpp

--- 575_Distribute_Candies.cpp
+++ 575_Distribute_Candies.cpp
@@ -1,3 +1,28 @@
+//set
+class Solution {
+public:
+    int distributeCandies(vector<int>& candies) {
+        set<int> s;
+        for(int i=0;i<candies.size()&&s.size()<candies.size()/2;++i)
+        {
+            s.insert(candies[i]);
+        }
+        return s.size();
+    }
+};
+class Solution {
+public:
+    int distributeCandies(vector<int>& candies) {
+        set<int> s;
+        for(int i=0;i<candies.size();++i)
+        {
+            s.insert(candies[i]);
+        }
+        return s.size()>candies.size()/2?candies.size()/2:s.size();
+    }
+};
+
+//简单哈希
 class Solution {
 public:
     int distributeCandies(vector<int>& candies) {
@@ -14,5 +39,16 @@ public:
         if(cnt>candies.size()/2)
             return candies.size()/2;
         else return cnt;
+    }
+};
+class Solution {
+public:
+    int distributeCandies(vector<int>& candies) {
+        unordered_map<int,int> mapping;
+        for(int i=0;i<candies.size()&&mapping.size()<candies.size()/2;++i)
+        {
+            mapping[candies[i]]=1;
+        }
+        return mapping.size();
     }
 };

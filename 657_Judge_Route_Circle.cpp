95b47b979881999f8b0ff641d617f00b8a39de27
yuexiuyawh <1173931023@qq.com>
2017-08-22 11:08:29 +0800

    Update 657_Judge_Route_Circle.cpp

--- 657_Judge_Route_Circle.cpp
+++ 657_Judge_Route_Circle.cpp
@@ -18,3 +18,21 @@ public:
         else return false;
     }
 };
+
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int v=0,h=0;
+        for(char c:moves)
+        {
+            switch(c)
+            {
+                case 'U': ++v;break;
+                case 'D': --v;break;
+                case 'R': ++h;break;
+                case 'L': --h;break;
+            }
+        }
+        return v==0&&h==0;
+    }
+};

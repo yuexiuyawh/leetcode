8d7f748d91fc68b17855e2644013f52a531997c0
yuexiuyawh <1173931023@qq.com>
2017-08-22 11:35:13 +0800

    Create 344_Reverse_String.cpp

---
+++ 344_Reverse_String.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string reverseString(string s) {
+        for(int i=0;i!=s.size()/2;++i)
+        {
+            char temp;
+            temp=s[i];
+            s[i]=s[s.size()-i-1];
+            s[s.size()-i-1]=temp;
+        }
+        return s;
+    }
+};

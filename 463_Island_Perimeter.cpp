6fea5a00fa96a8ef6f4ed8f5281c13a90b11cbfe
yuexiuyawh <1173931023@qq.com>
2017-08-22 12:25:09 +0800

    Update 463_Island_Perimeter.cpp

--- 463_Island_Perimeter.cpp
+++ 463_Island_Perimeter.cpp
@@ -16,3 +16,23 @@ public:
         return cnt;
     }
 };
+
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int cnt=0,repeat=0;
+        for(int i=0;i!=grid.size();++i)
+        {
+            for(int j=0;j!=grid[i].size();++j)
+            {
+                if(grid[i][j]==1)
+                {
+                    ++cnt;
+                    if(i!=0&&grid[i-1][j]==1) ++repeat;
+                    if(j!=0&&grid[i][j-1]==1) ++repeat;
+                }
+            }
+        }
+        return cnt*4-2*repeat;
+    }
+};

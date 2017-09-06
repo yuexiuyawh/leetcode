7a69adeb948e908c0357e3dbad641d51112290f3
yuexiuyawh <1173931023@qq.com>
2017-09-06 17:16:39 +0800

    Update 637_Average_of_Levels_in_Binary_Tree.cpp

--- 637_Average_of_Levels_in_Binary_Tree.cpp
+++ 637_Average_of_Levels_in_Binary_Tree.cpp
@@ -1,3 +1,4 @@
+//深度优先
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -33,6 +34,40 @@ public:
         vector<double> res;
         for(int i=0;i!=sum.size();++i)
             res.push_back(sum[i]/cnt[i]);
+        return res;
+    }
+};
+
+//广度优先
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> res;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            long sum=0;
+            int size=q.size();
+            for(int i=0;i!=size;++i)
+            {
+                TreeNode* t=q.front();
+                q.pop();
+                sum=sum+t->val;
+                if(t->left) q.push(t->left);
+                if(t->right) q.push(t->right);
+            }
+            res.push_back((double)sum/size);
+        }
         return res;
     }
 };

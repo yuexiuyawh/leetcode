fbd4cbc37cf8b733889a62ea27255fdec7dca4e8
yuexiuyawh <1173931023@qq.com>
2017-09-12 16:40:16 +0800

    Update 104_Maximum_Depth_of_Binary_Tree.cpp

--- 104_Maximum_Depth_of_Binary_Tree.cpp
+++ 104_Maximum_Depth_of_Binary_Tree.cpp
@@ -1,3 +1,4 @@
+//深度优先
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -15,5 +16,38 @@ public:
         int left=maxDepth(root->left)+1;
         int right=maxDepth(root->right)+1;
         return left>right?left:right;
+    }
+};
+
+//广度优先
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root) return 0;
+        queue<TreeNode *> q;
+        int cnt=0;
+        q.push(root);
+        while(q.size())
+        {
+            ++cnt;
+            int k=q.size();
+            for(int i=0;i!=k;++i)
+            {
+                TreeNode* t=q.front();
+                q.pop();
+                if(t->left) q.push(t->left);
+                if(t->right) q.push(t->right);
+            } 
+        }
+        return cnt;
     }
 };

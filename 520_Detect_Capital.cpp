5ce4d28a8a9f3ecf9b5cc24eecdbc79ae2c9c994
yuexiuyawh <1173931023@qq.com>
2017-09-12 17:01:22 +0800

    Create 520_Detect_Capital.cpp

---
+++ 520_Detect_Capital.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int mark=0;
+        if(word[0]>='a'&&word[0]<='z')
+        {
+            for(int i=1;i!=word.size();++i)
+            {
+                if(word[i]>='A'&&word[i]<='Z')
+                {
+                    mark=1;
+                    break;
+                }
+            }
+            if(mark) return false;
+            else return true;
+        }
+        else if(word.size()==1)
+            return true;
+        else if(word[1]>='a'&&word[1]<='z')
+        {
+            for(int i=2;i!=word.size();++i)
+            {
+                if(word[i]>='A'&&word[i]<='Z')
+                {
+                    mark=1;
+                    break;
+                }
+            }
+            if(mark) return false;
+            else return true;
+        }
+        else
+        {
+            for(int i=2;i!=word.size();++i)
+            {
+                if(word[i]>='a'&&word[i]<='z')
+                {
+                    mark=1;
+                    break;
+                }
+            }
+            if(mark) return false;
+            else return true;
+        }
+    }
+};

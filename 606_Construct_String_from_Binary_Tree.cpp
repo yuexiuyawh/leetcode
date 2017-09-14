d7b124bec67c79b9a03812d40c0f22caf2e2d0c7
yuexiuyawh <1173931023@qq.com>
2017-09-14 20:26:19 +0800

    Create 606_Construct_String_from_Binary_Tree.cpp

---
+++ 606_Construct_String_from_Binary_Tree.cpp
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    string tree2str(TreeNode* t) {
+        if(!t) return"";
+        if(!t->left&&!t->right) return to_string(t->val);
+        else if(t->left&&!t->right) return to_string(t->val)+"("+tree2str(t->left)+")";
+        else return to_string(t->val)+"("+tree2str(t->left)+")("+tree2str(t->right)+")";
+    }
+};

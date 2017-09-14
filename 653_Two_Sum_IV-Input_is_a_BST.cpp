c07bcb83e48687995bab63b411eae533e6a993d2
yuexiuyawh <1173931023@qq.com>
2017-09-14 17:17:56 +0800

    Update 653_Two_Sum_IV-Input_is_a_BST.cpp

--- 653_Two_Sum_IV-Input_is_a_BST.cpp
+++ 653_Two_Sum_IV-Input_is_a_BST.cpp
@@ -32,3 +32,70 @@ public:
         return mark;
     }
 };
+
+//深度优先搜索
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool traversal(TreeNode* root,int k,set<int> *s)
+    {
+        set<int>::iterator it;
+        if(!root) return false;
+        it=s->find(root->val);
+        if(it!=s->end())
+            return true;
+        s->insert(k-root->val);
+        return traversal(root->left,k,s)||traversal(root->right,k,s);
+    }
+    bool findTarget(TreeNode* root, int k) {
+        set<int> s;
+        return traversal(root,k,&s);
+    }
+};
+
+//指针
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void traversal(TreeNode* root,int k,vector<int> &nums)
+    {
+        if(!root)
+            return;
+        traversal(root->left,k,nums);
+        nums.push_back(root->val);
+        traversal(root->right,k,nums);
+        return;
+    }
+    bool findTarget(TreeNode* root, int k) {
+        vector<int> nums;
+        traversal(root,k,nums);
+        int i=0;
+        int j=nums.size()-1;
+        while(i<j)
+        {
+            if(nums[i]+nums[j]==k)
+                return true;
+            else if(nums[i]+nums[j]<k)
+                ++i;
+            else 
+                --j;
+        }
+        return false;
+    }
+};

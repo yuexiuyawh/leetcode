6b28320659277da543e86a2355b1195bc41f2672
yuexiuyawh <1173931023@qq.com>
2017-09-19 17:46:09 +0800

    Create 217_Contains_Duplicate.cpp

---
+++ 217_Contains_Duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> m;
+        for(int n:nums)
+        {
+            if(m[n]==1) return true;
+            m[n]=1;
+        }
+        return false;
+    }
+};
